c73466d665fcb3e31f1dfda0a787f4e463d51b9c
yohanes.bimo <yohanes.bimo@administrators-MacBook-Pro.local>
2018-09-20 10:31:53 +0700

    fix file close photo

--- user/func_test.go
+++
@@ -1,9 +0,0 @@
-package user
-
-/*func TestHashPassword(t *testing.T) {
-	hash := hashPassword("shopee")
-	expected := "$2a$10$xCDj15HhJ8FsTfH5nD.nfuWd0MDL9GW3qgWCd8QiYNFBlTNupVPwy"
-	if hash != expected {
-		t.Errorf("Incorrect hash value, got %s, want %s", hash, expected)
-	}
-}*/
